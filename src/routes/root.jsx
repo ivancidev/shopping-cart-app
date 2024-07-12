f4f3de1ccdb99839177fa731969e29e821bb5fcb
Herlan <herlanherbas@gmail.com>
2024-07-12 17:24:25 -0400

    feat: create root.jsx

---
+++ src/routes/root.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+
+export default function Root() {
+  return (
+    <>
+      <Navbar />
+      <div id="detail">
+        <Outlet />
+      </div>
+    </>
+  );
+}
