4107522143a173b96eeb84913ed9898274e2f8c8
Herlan <herlanherbas@gmail.com>
2024-07-12 22:45:30 -0400

    update Products.jsx

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -2,13 +2,15 @@ import React, { useContext } from "react";
 import CardProduct from "../../components/Card/CardProduct";
 import "./styles/products.css";
 import { ProductsContext } from "../../context/ProductsContext";
+import { ShoppingContext } from "../../context/ShoppingContext";
 
 export default function Products() {
   const { products } = useContext(ProductsContext);
+  const { addShopping } = useContext(ShoppingContext);
   return (
     <section className="products">
       {products.map((product) => (
-        <CardProduct key={product.id} {...product} />
+        <CardProduct key={product.id} {...product} handleAddToCart={() => addShopping(product)} />
       ))}
     </section>
   );
