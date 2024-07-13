1c672e3f72d6d6a3ae63532c55bbadbb32b655de
Herlan <herlanherbas@gmail.com>
2024-07-12 21:05:25 -0400

    feat: create ProductsProvider.jsx

---
+++ src/context/ProductsProvider.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import { ProductsContext } from "./ProductsContext";
+
+export default function ProductsProvider({ children }) {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
+    setProducts(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  return (
+    <ProductsContext.Provider value={{ products }}>
+      {children}
+    </ProductsContext.Provider>
+  );
+}
