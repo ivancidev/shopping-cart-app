4c85b3d3ef1a5a5dad6c2eb3dcf0b9677b6344d8
Herlan <herlanherbas@gmail.com>
2024-07-12 20:09:05 -0400

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Root from "./routes/root"
+import ErrorPage from "./routes/error-page"
+import Products from "./pages/Products"
+import Shopping from "./pages/Shopping"
 
 function App() {
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Root />
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Products />
+        },
+        {
+          path: "/shopping",
+          element: <Shopping />
+        }
+      ]
     }
   ])
 
